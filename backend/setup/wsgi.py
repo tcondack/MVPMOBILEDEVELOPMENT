0c049982f2d1f73eb5e2c3ecd5a5ce185ee249fd
Tiago <tiagocondack@gmail.com>
2026-08-28 14:51:41 -0300

    correão produção

--- backend/setup/wsgi.py
+++ backend/setup/wsgi.py
@@ -1,14 +1,7 @@
-"""
-WSGI config for setup project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
